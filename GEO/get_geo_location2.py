18707f6ce08eef805ad116865828129bcaf8315e
disperx <src@vp.pl>
2017-06-11 23:15:14 +0200

    Create get_geo_location2.py

--- GEO/get_geo_location2.py
+++ GEO/get_geo_location2.py
@@ -1,7 +1,8 @@
 # remember to pip install geopy
 from geopy.geocoders import Nominatim
 geolocator = Nominatim()
-my_address="Bielsk Podlaski"
+my_address = input("Enter new address to check: ")
 location = geolocator.geocode(my_address, timeout=2)
 print(location.address)
 print(location.latitude, location.longitude)
+print("\n")
